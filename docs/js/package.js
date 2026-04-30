6df091e9355a230c5c2281f0a010bbc70a25546f
3wlh <3wlh@3wlh.cn>
2026-04-30 14:55:01 +0800

    up

---
+++ docs/js/package.js
@@ -0,0 +1,5 @@
+var package = {
+    "luci-app-napcatapi": "图片编辑",
+    "luci-app-mddns": "多播DNS",
+    "luci-app-docker": "luci-app-docker"
+};
---
+++ docs/js/packages.js
@@ -0,0 +1,37 @@
+var versions = {
+  "openwrt": [
+    "24.10-SNAPSHOT",
+    "24.10.6",
+    "24.10.5",
+    "24.10.4",
+    "24.10.3",
+    "24.10.2",
+    "24.10.1",
+    "24.10.0",
+    "23.05.5"
+  ],
+  "immortalwrt": [
+    "24.10-SNAPSHOT",
+    "24.10.6",
+    "24.10.5",
+    "24.10.4",
+    "24.10.3",
+    "24.10.2",
+    "24.10.1",
+    "24.10.0",
+    "23.05.5"
+  ]
+};
+
+var packages = {
+  "luci-app-mddns": "多动态DNS",
+  "luci-app-diskman": "磁盘管理",
+  "luci-app-filetransfer": "文件传输",
+  "luci-app-firewall": "防火墙设置",
+  "luci-app-frpc": "内网穿透 Frp 客户端",
+  "luci-app-frps": "内网穿透 Frp 服务端",
+  "luci-app-openvpn-server": "OpenVPN 服务器",
+  "luci-app-passwall": "PassWall 科学上网",
+  "luci-app-ttyd": "网页终端",
+  "luci-theme-argon": "主题Argon",
+};
